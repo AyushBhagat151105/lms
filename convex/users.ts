27b018ac145542a32a2e796decfd91d3fd573244
Ayush_Bhagat <103896902+AyushBhagat151105@users.noreply.github.com>
2025-07-04 15:55:59 +0530

    feat: add queries for retrieving users by clerk ID and Stripe customer ID, and implement user access validation

--- convex/users.ts
+++ convex/users.ts
@@ -29,3 +29,78 @@ export const createUser = mutation({
     return userId;
   },
 });
+
+export const getUserByClerkId = query({
+  args: {
+    clerkId: v.string(),
+  },
+  handler: async (ctx, args) => {
+    return await ctx.db
+      .query("users")
+      .withIndex("by_clerkId", (q) => q.eq("clerkId", args.clerkId))
+      .unique();
+  },
+});
+
+export const getUserByStripeCustomerId = query({
+  args: {
+    stripeCustomerId: v.string(),
+  },
+  handler: async (ctx, args) => {
+    return await ctx.db
+      .query("users")
+      .withIndex("by_stripeCustomerId", (q) =>
+        q.eq("stripeCustomerId", args.stripeCustomerId)
+      )
+      .unique();
+  },
+});
+
+export const getUserAccess = query({
+  args: { userId: v.id("users"), courseId: v.id("courese") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new ConvexError("Unauthorized");
+    }
+
+    const user = await ctx.db.get(args.userId);
+    if (!user) {
+      throw new ConvexError("User not found");
+    }
+
+    //   Check for the subscription access
+    if (user.currentSubscriptionId) {
+      const subscription = await ctx.db.get(user.currentSubscriptionId);
+      if (subscription && subscription.status === "active") {
+        return {
+          hasAccess: true,
+          accessType: "subscription",
+          message: "User has an active subscription.",
+        };
+      }
+    }
+
+    //   Check for course purchase
+    const purchase = await ctx.db
+      .query("purchases")
+      .withIndex("by_userId_and_courseId", (q) =>
+        q.eq("userId", args.userId).eq("courseId", args.courseId)
+      )
+      .unique();
+
+    if (purchase) {
+      return {
+        hasAccess: true,
+        accessType: "course",
+      };
+    }
+
+    return {
+      hasAccess: false,
+      accessType: null,
+      message: "User does not have access to this course.",
+    };
+  },
+});
